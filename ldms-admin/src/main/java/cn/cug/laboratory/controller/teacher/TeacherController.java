e974cc59fd457e1548074f8ac7e99bac69c0b089
HHcoder <1475033744@qq.com>
2016-05-27 11:24:10 +0800

    把所有的测试类写出来,Lab 的流程增加一个函数

--- ldms-admin/src/main/java/cn/cug/laboratory/controller/teacher/TeacherController.java
+++ ldms-admin/src/main/java/cn/cug/laboratory/controller/teacher/TeacherController.java
@@ -1,5 +1,7 @@
 package cn.cug.laboratory.controller.teacher;
 
+import cn.cug.laboratory.model.extend.ProjectExtend;
+import cn.cug.laboratory.model.persistent.Teacher;
 import cn.cug.laboratory.model.persistent.User;
 import cn.cug.laboratory.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,12 @@ public class TeacherController {
     @Autowired
     private UserService userService;
 
+    /**
+     * session中保存用户参数为user
+     * @param session
+     * @param user
+     * @return
+     */
     @RequestMapping(value = {"/login"}, method = RequestMethod.POST)
     public ModelAndView login(HttpSession session, User user) {
         ModelAndView mav = new ModelAndView();
@@ -52,5 +60,26 @@ public class TeacherController {
         else
             result = "Error";
         return result;
+    }
+
+    /**
+     *
+     * @param project
+     * @return
+     * 申请实验第一步，提交信息，在controlller里面保存用户提交的信息，并不存入数据库
+     * 要生成一个实验ID
+     * 根据传过来的实验室名称查询lab_id
+     * 将实验状态设置为0,标识为提交状态
+     *从当前session里面获取tea_id
+     *将上传文件保存到临时文件夹(在确认信息无误,确认申请以后放到upload文件夹)
+     */
+    @RequestMapping(value = "saveprojectinfo",method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String saveprojectinfo(ProjectExtend project,HttpSession session){
+        project.setState(0);
+        project.setTeaId(((Teacher)session.getAttribute("user")).getId());
+
+        return "sucess";
     }
 }
---
+++ ldms-service/src/main/java/cn/cug/laboratory/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,9 @@
+package cn.cug.laboratory.service.impl;
+
+import cn.cug.laboratory.service.TeacherService;
+
+/**
+ * Created by HXY on 2016/5/27.
+ */
+public class TeacherServiceImpl implements TeacherService {
+}
