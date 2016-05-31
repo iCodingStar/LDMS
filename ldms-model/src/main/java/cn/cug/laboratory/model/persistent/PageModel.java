accf154558496ac161468e247a02fb29afd53e7c
iCodingStar <codingstar@qq.com>
2016-05-31 17:25:00 +0800

    admin新提交

--- ldms-model/src/main/java/cn/cug/laboratory/model/persistent/PageModel.java
+++ ldms-model/src/main/java/cn/cug/laboratory/model/persistent/PageModel.java
@@ -41,37 +41,41 @@ public class PageModel<T> {
         return currentPage;
     }
 
-    public PageModel(Integer currentPage,Integer offset,Integer totalRecords){
-        //初始化当前页面
-        if (currentPage == null){
-            currentPage = 1;
-        }
-        if (currentPage <= 1){
-            this.currentPage = 1;
-        }
-        else if (currentPage > totalPages){
-            this.currentPage = currentPage;
-        }
-        //初始化起始点
-        this.startPosition = (this.currentPage-1) * offset;
+    public PageModel(Integer currentPage, Integer offset, Integer totalRecords) {
         //初始化offset
-        if (offset <= 0){
+        if (offset <= 0) {
             this.offset = 0;
         }
         this.offset = offset;
+
         //初始化总记录数
         this.totalRecords = totalRecords;
+
         //初始化总页面数
         if (totalRecords % offset == 0)
             this.totalPages = totalRecords / offset;
         else
             this.totalPages = totalRecords / offset + 1;
+
+        //初始化当前页面
+        if (currentPage == null) {
+            currentPage = 1;
+        }
+        if (currentPage <= 1) {
+            this.currentPage = 1;
+        } else if (currentPage > this.totalPages) {
+            this.currentPage = this.totalPages;
+        }
+
+        //初始化起始点
+        this.startPosition = (this.currentPage - 1) * offset;
+
         //初始化起始结束页面
-        if (totalPages <= 10){
+        if (totalPages <= 10) {
             this.startPage = 1;
             this.endPage = this.totalPages;
-        }else {
-            this.startPage = currentPage - currentPage % 10 + 1;
+        } else {
+            this.startPage = this.currentPage - this.currentPage % 10 + 1;
             this.endPage = this.startPage + 10;
         }
     }
